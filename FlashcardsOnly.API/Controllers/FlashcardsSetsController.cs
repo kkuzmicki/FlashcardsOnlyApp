963d4f6e1a36ef8738c694ef71dbd1188a9f2b32
kkuzmicki <kkuzmicki@wp.pl>
2024-05-26 02:32:41 +0200

    First commit

---
+++ FlashcardsOnly.API/Controllers/FlashcardsSetsController.cs
@@ -0,0 +1,21 @@
+﻿using FlashcardsOnly.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlashcardsOnly.API.Controllers;
+
+[ApiController]
+[Route("api/flashcardsSets")]
+public class FlashcardsSetsController : ControllerBase
+{
+    [HttpGet]
+    public ActionResult<IEnumerable<FlashcardsSetDTO>> GetFlashcardsSets()
+    {
+        return NotFound();
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<FlashcardsSetDTO> GetFlashcardsSet(int id)
+    {
+        return Ok();
+    }
+}
---
+++ FlashcardsOnly.API/Models/FlashcardsSetDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace FlashcardsOnly.API.Models
+{
+    public class FlashcardsSetDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+
+    }
+}
